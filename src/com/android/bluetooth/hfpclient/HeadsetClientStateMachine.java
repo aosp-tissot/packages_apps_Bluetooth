CyanogenMod/android_packages_apps_Bluetooth

HF: PTS Issue TC_HF_RSV_BV_01_I Fix
When volume changed from HF Device, it is unable to send volume change
intent from HeadsetClientservice to HesetClentStatemachine.So, AT command
regarding Volume changes will not be able to send to AG device.changed
streamType check from STREAM_VOICE_CALL to STREAM_BLUETOOTH_SCO.

CRs-Fixed: 1027595
Change-Id: Icbf501c304be1ae0d675d4f7926acff26f783ae2

src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
@@ -1233,8 +1233,8 @@ final class HeadsetClientStateMachine extends StateMachine {
         mIndicatorCallSetup = -1;
         mIndicatorCallHeld = -1;
 
-        mMaxAmVcVol = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL);
-        mMinAmVcVol = mAudioManager.getStreamMinVolume(AudioManager.STREAM_VOICE_CALL);
+        mMaxAmVcVol = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_BLUETOOTH_SCO);
+        mMinAmVcVol = mAudioManager.getStreamMinVolume(AudioManager.STREAM_BLUETOOTH_SCO);
 
         mOperatorName = null;
         mSubscriberInfo = null;
@@ -1528,7 +1528,7 @@ final class HeadsetClientStateMachine extends StateMachine {
                     }
                     transitionTo(mConnected);
 
-                    int amVol = mAudioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL);
+                    int amVol = mAudioManager.getStreamVolume(AudioManager.STREAM_BLUETOOTH_SCO);
                     sendMessage(
                             obtainMessage(HeadsetClientStateMachine.SET_SPEAKER_VOLUME, amVol, 0));
                     // Mic is either in ON state (full volume) or OFF state. There is no way in
@@ -1885,7 +1885,7 @@ final class HeadsetClientStateMachine extends StateMachine {
                                 Log.d(TAG, "AM volume set to " +
                                       hfToAmVol(event.valueInt2));
                                 mAudioManager.setStreamVolume(
-                                    AudioManager.STREAM_VOICE_CALL,
+                                    AudioManager.STREAM_BLUETOOTH_SCO,
                                     hfToAmVol(event.valueInt2),
                                     AudioManager.FLAG_SHOW_UI);
                                 mVgsFromStack = true;
@@ -2108,7 +2108,7 @@ final class HeadsetClientStateMachine extends StateMachine {
                     // We need to set the volume after switching into HFP mode as some Audio HALs
                     // reset the volume to a known-default on mode switch.
                     final int amVol =
-                            mAudioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL);
+                              mAudioManager.getStreamVolume(AudioManager.STREAM_BLUETOOTH_SCO);
                     final int hfVol = amToHfVol(amVol);
 
                     Log.d(TAG,"hfp_enable=true");

aosp-tissot/packages_apps_Bluetooth

src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
@@ -683,8 +683,8 @@ public class HeadsetClientStateMachine extends StateMachine {
         mIndicatorNetworkSignal = 0;
         mIndicatorBatteryLevel = 0;
 
-        mMaxAmVcVol = sAudioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL);
-        mMinAmVcVol = sAudioManager.getStreamMinVolume(AudioManager.STREAM_VOICE_CALL);
+        mMaxAmVcVol = sAudioManager.getStreamMaxVolume(AudioManager.STREAM_BLUETOOTH_SCO);
+        mMinAmVcVol = sAudioManager.getStreamMinVolume(AudioManager.STREAM_BLUETOOTH_SCO);
 
         mOperatorName = null;
         mSubscriberInfo = null;
@@ -1003,7 +1003,7 @@ public class HeadsetClientStateMachine extends StateMachine {
                     }
                     transitionTo(mConnected);
 
-                    int amVol = sAudioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL);
+                    int amVol = sAudioManager.getStreamVolume(AudioManager.STREAM_BLUETOOTH_SCO);
                     sendMessage(
                             obtainMessage(HeadsetClientStateMachine.SET_SPEAKER_VOLUME, amVol, 0));
                     // Mic is either in ON state (full volume) or OFF state. There is no way in
@@ -1314,7 +1314,7 @@ public class HeadsetClientStateMachine extends StateMachine {
                             if (event.valueInt == HeadsetClientHalConstants.VOLUME_TYPE_SPK) {
                                 mCommandedSpeakerVolume = hfToAmVol(event.valueInt2);
                                 Log.d(TAG, "AM volume set to " + mCommandedSpeakerVolume);
-                                sAudioManager.setStreamVolume(AudioManager.STREAM_VOICE_CALL,
+                                sAudioManager.setStreamVolume(AudioManager.STREAM_BLUETOOTH_SCO,
                                         +mCommandedSpeakerVolume, AudioManager.FLAG_SHOW_UI);
                             } else if (event.valueInt
                                     == HeadsetClientHalConstants.VOLUME_TYPE_MIC) {
@@ -1428,7 +1428,7 @@ public class HeadsetClientStateMachine extends StateMachine {
 
                     // We need to set the volume after switching into HFP mode as some Audio HALs
                     // reset the volume to a known-default on mode switch.
-                    final int amVol = sAudioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL);
+                    final int amVol = sAudioManager.getStreamVolume(AudioManager.STREAM_BLUETOOTH_SCO);
                     final int hfVol = amToHfVol(amVol);
 
                     if (DBG) {